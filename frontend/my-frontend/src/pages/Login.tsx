c3edb7325893cc079a6384190d61ae3195214dc7
manojkumar-13-official <manojkumaratarumugam@gmail.com>
2025-12-02 21:58:33 +0530

    The final completion of frontend designing for Authentication project

--- frontend/my-frontend/src/pages/Login.tsx
+++ frontend/my-frontend/src/pages/Login.tsx
@@ -4,12 +4,14 @@ const Login = () => {
   return (
     <div>
       <div className="relative flex justify-center items-center h-screen flex-col gap-y-10">
-        <h1 className="text-gray-300 text-2xl font-bold">Login</h1>
-        <div className="flex flex-col inputbox">
+        <div className="border-2 border-green-500 rounded-2xl flex flex-col h-120 w-100 p-10">
+        <h1 className="text-green-500 text-2xl font-bold text-center">Login</h1>
+        <div className="flex flex-col inputbox mt-10 ml-1">
           <input
             type="email"
             name="email"
             id=""
+            placeholder=" "
             className="border-2 border-gray-500 outline-none pl-2.5 text-white h-10 w-75 rounded-xl focus:border-green-500"
           />
           <label
@@ -19,12 +21,13 @@ const Login = () => {
             Email
           </label>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col inputbox mt-10 ml-1">
           <input
             type="text"
             name="password"
             id=""
-            className="border-2 border-gray-500 outline-none pl-2.5 text-white h-10 w-75 rounded-xl"
+            placeholder=" "
+            className="border-2 border-gray-500 outline-none pl-2.5 text-white h-10 w-75 rounded-xl focus:border-green-500 "
           />
           <label
             htmlFor="password"
@@ -41,16 +44,17 @@ const Login = () => {
 
         <button
           type="submit"
-          className="text-white bg-green-500 h-10 w-75 rounded-2xl font-semibold text-xl text-center cursor-pointer hover:bg-green-600 outline-none"
+          className="text-white bg-green-500 mt-10 ml-1 h-10 w-75 rounded-2xl font-semibold text-xl text-center cursor-pointer hover:bg-green-600 outline-none"
         >
           Login
         </button>
-        <h1 className="text-lg text-white">
+        <h1 className="text-lg text-gray-400 mt-5 text-center">
           Don't have an account ?{" "}
           <a href="/register" className="text-green-400 underline">
             Register
           </a>
         </h1>
+      </div>
       </div>
     </div>
   );
--- frontend/my-frontend/src/pages/Register.tsx
+++ frontend/my-frontend/src/pages/Register.tsx
@@ -3,14 +3,16 @@ import React from 'react'
 const Register = () => {
   return (
     <div>
-      <div className="relative flex justify-center items-center h-screen flex-col gap-y-10">
-        <h1 className="text-gray-300 text-2xl font-bold">Register</h1>
-        <div className="flex flex-col">
+      <div className="relative flex justify-center items-center h-screen">
+        <div className="border-2 border-green-500 rounded-2xl flex flex-col h-120 w-100 p-10">
+        <h1 className="text-green-500 text-2xl font-bold text-center">Register</h1>
+        <div className="flex flex-col mt-10 ml-1 inputbox">
           <input
             type="text"
             name="name"
             id=""
-            className="border-2 border-gray-500 outline-none pl-2.5 text-white h-10 w-75 rounded-xl bg-black"
+            placeholder=" "
+            className="border-2 border-gray-500 outline-none pl-2.5 text-white h-10 w-75 rounded-xl focus:border-green-500"
           />
           <label
             htmlFor="name"
@@ -19,12 +21,13 @@ const Register = () => {
             Name
           </label>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col mt-10 ml-1 inputbox">
           <input
             type="email"
             name="email"
             id=""
-            className="border-2 border-gray-500 outline-none pl-2.5 text-white h-10 w-75 rounded-xl"
+            placeholder=" "
+            className="border-2 border-gray-500 outline-none pl-2.5 text-white h-10 w-75 rounded-xl focus:border-green-500"
           />
           <label
             htmlFor="email"
@@ -33,12 +36,13 @@ const Register = () => {
             Email
           </label>
         </div>
-        <div className="flex flex-col">
+        <div className="flex flex-col mt-10 ml-1 inputbox">
           <input
             type="text"
             name="password"
             id=""
-            className="border-2 border-gray-500 outline-none pl-2.5 text-white h-10 w-75 rounded-xl"
+            placeholder=" "
+            className="border-2 border-gray-500 outline-none pl-2.5 text-white h-10 w-75 rounded-xl focus:border-green-500"
           />
           <label
             htmlFor="password"
@@ -48,8 +52,9 @@ const Register = () => {
           </label>
         </div>
 
-        <button type="submit" className="text-white bg-green-500 h-10 w-75 rounded-2xl font-semibold text-xl text-center cursor-pointer hover:bg-green-600 outline-none">Register</button>
-        <h1 className="text-lg text-white">Already have an account ? <a href="/" className="text-green-400 underline">Login</a></h1>
+        <button type="submit" className="text-white bg-green-500 mt-10 ml-1 h-10 w-75 rounded-2xl font-semibold text-xl text-center cursor-pointer hover:bg-green-600 outline-none">Register</button>
+        <h1 className="text-lg text-center mt-2 text-gray-400">Already have an account ? <a href="/" className="text-green-400 underline">Login</a></h1>
+      </div>
       </div>
     </div>
   )
